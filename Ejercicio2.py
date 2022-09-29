b829b48114f706b3a18376f5505b9823a0df6f03
dsalle03 <dsalle2003@gmail.com>
2022-09-30 01:36:44 +0200

    Cambio 4

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -6,10 +6,12 @@
 # Finalmente muestra el valor final del numero_magico por pantalla
 
 
-numero_magico = 12345679
-numero_usuario = input(print("Dime un número del 1 al 9: "))
-print("El número del usuario es " + numero_usuario)
-numero_usuario = numero_usuario*9
-numero_magico = numero_magico*numero_usuario
-print(numero_magico)
-
+while True:
+    try:
+        numero_magico = 12345679
+        numero_usuario = int(input(print("Dime un número del 1 al 9: ")))
+        numero_usuario_2 = numero_usuario*9
+        numero_magico_2 = numero_magico*numero_usuario_2
+    finally:
+        print(numero_magico_2)
+        break
