a66e81017c0f9dc8a8e129021bc13be5dc9cfee6
dsalle03 <dsalle2003@gmail.com>
2022-10-01 12:19:40 +0200

    Cambio final

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,10 +1,8 @@
-while True:
-    try:
-        numero_magico = 12345679
-        numero_usuario = int(input(print("Dime un número del 1 al 9: ")))
-        numero_usuario_2 = numero_usuario*9
-        numero_magico_2 = numero_magico*numero_usuario_2
-        print(numero_magico_2)
-        break
-    except ValueError:
-        print("Solo se aceptan números")
+numero_magico = 12345679
+numero_usuario = int(input(print("Dime un número del 1 al 9: ")))
+if numero_usuario in range (10):
+    numero_usuario_2 = numero_usuario**9
+    print(numero_magico*numero_usuario_2)
+else:
+    print("El número tiene que ser del 1 al 9")
+
